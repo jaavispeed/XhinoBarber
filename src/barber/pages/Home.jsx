5e48221c1752b1308ee369a7d1f88b06a0472b87
jaavispeed <ja.rodriguezc@duocuc.cl>
2025-03-05 17:05:23 -0300

    Logo de barberia OK

--- src/barber/pages/Home.jsx
+++ src/barber/pages/Home.jsx
@@ -1,6 +1,10 @@
+import barberlogo from "../../assets/barberlogo.png";
 
 export const LandingPage = () => {
   return (
-    <h1>Soy Jose Moya, barbero comprometido con ofrecer cortes de pelo de calidad.</h1>
+    
+    <h1>
+      Soy Jose Moya, barbero comprometido con ofrecer cortes de pelo de calidad.</h1>
+    
   )
 }
--- src/shared/components/Navbar.jsx
+++ src/shared/components/Navbar.jsx
@@ -1,13 +1,20 @@
 import React from 'react';
 import { Link, NavLink } from 'react-router-dom';
-import './Navbar.css'; // Asegúrate de importar el archivo CSS
+import './Navbar.css';
+import barberlogo from "../../assets/barberlogo.png";
 
 export const Navbar = () => {
   return (
     <nav className="navbar navbar-expand-sm navbar-dark">
       <div className="container-fluid">
         <div className="navbar-collapse d-flex justify-content-between w-100">
+
           <Link className="navbar-brand mx-auto" to="/">
+          <img 
+            src={barberlogo} 
+            alt="XhinoBarber" 
+            className="navbar-brand-img"  // Agregar clase para estilizar
+          />
             XhinoBarber
           </Link>
 
@@ -21,6 +28,7 @@ export const Navbar = () => {
             </NavLink>
           </div>
         </div>
+        
       </div>
     </nav>
   );
