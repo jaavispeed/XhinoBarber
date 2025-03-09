e5b47d1e3d50928bca5374d1f94fcbcd714ed43f
Matias <mgcm03@hotmail.com>
2025-03-09 19:43:25 -0300

    Formulario

--- src/components/Formulario.jsx
+++ src/components/Formulario.jsx
@@ -1,9 +1,38 @@
 import "../css/Formulario.css";
 
 export const Formulario = () => {
+
+
+
   return (
-    <div className="centered">
-      <h1>Formulario</h1>
-    </div>
+    <>
+      <div className="centered">
+        
+        <h1>Formulario</h1>
+        <div className="form-container">
+          <form className="form">
+
+          <div className="form-row">
+            <div>
+              <h4>Nombre:</h4>
+              <input type="text"/>
+            </div>
+            <div>
+              <h4>Apellido:</h4>
+              <input type="text"/>
+            </div>
+
+          </div>
+            <h4>Telefono: </h4>
+            <input type="text"/>
+
+            <h4>Correo: </h4>
+            <input type="text"/>
+
+          </form>
+        </div>
+      </div>
+    </>
+
   );
 };
