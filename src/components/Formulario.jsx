d8e6580393c3eb67b75160153074145847ab0688
jaavispeed <ja.rodriguezc@duocuc.cl>
2025-03-12 22:55:34 -0300

    Pop up mientras

--- src/components/Formulario.jsx
+++ src/components/Formulario.jsx
@@ -156,6 +156,8 @@ export const Formulario = () => {
         try {
             const response = await axios.post("https://backend-xhino-barber.vercel.app/api/agendamiento", formData);
             console.log("Formulario enviado con éxito:", response.data);
+            alert("Gracias por agendar tu cita, por favor revisa tu correo para confirmar tu asistencia.");
+
 
             setName("");
             setLastName("");
