400a5c28294a89bde5cb01e25cfed8013cd30bd4
jaavispeed <ja.rodriguezc@duocuc.cl>
2025-03-12 22:53:15 -0300

    Modificacion del formulario

--- src/components/Formulario.jsx
+++ src/components/Formulario.jsx
@@ -23,14 +23,14 @@ export const Formulario = () => {
         switch (field) {
             case 'name':
                 if (!/^[A-Za-zÁÉÍÓÚáéíóúñÑ\s]+$/.test(name)) {
-                    newErrors.name = "El nombre solo puede contener letras y espacios.";
+                    newErrors.name = "El nombre es obligatorio.";
                 } else {
                     delete newErrors.name;
                 }
                 break;
             case 'lastName':
                 if (!/^[A-Za-zÁÉÍÓÚáéíóúñÑ\s]+$/.test(lastName)) {
-                    newErrors.lastName = "El apellido solo puede contener letras y espacios.";
+                    newErrors.lastName = "El apellido es obligatorio.";
                 } else {
                     delete newErrors.lastName;
                 }
@@ -77,39 +77,39 @@ export const Formulario = () => {
 
     const validate = () => {
         let newErrors = {};
-    
+
         // Validar nombre
         if (!/^[A-Za-zÁÉÍÓÚáéíóúñÑ\s]+$/.test(name)) {
             newErrors.name = "El nombre solo puede contener letras y espacios.";
         }
-    
+
         // Validar apellido
         if (!/^[A-Za-zÁÉÍÓÚáéíóúñÑ\s]+$/.test(lastName)) {
             newErrors.lastName = "El apellido solo puede contener letras y espacios.";
         }
-    
+
         // Validar teléfono
         if (!/^\d{9}$/.test(phone)) {
             newErrors.phone = "El teléfono debe contener 9 dígitos.";
         }
-    
+
         // Validar correo electrónico
         if (!/^[^\s@]+@[^\s@]+\.[^\s@]+$/.test(email)) {
             newErrors.email = "El correo electrónico no es válido.";
         }
-    
+
         // Validar fecha
         if (selectedDate.date) {
             const today = new Date();
             today.setHours(0, 0, 0, 0); // Resetear la hora de hoy a medianoche para comparación de fechas
-        
+
             const [year, month, day] = selectedDate.date.split("-").map(Number);
             const selectedDateObj = new Date(year, month - 1, day); // Crear la fecha seleccionada correctamente
-        
+
             if (selectedDateObj.getTime() < today.getTime()) {
                 newErrors.date = "La fecha no puede ser anterior a hoy.";
             }
-        
+
             // Validar la hora solo si la fecha es hoy
             if (selectedDateObj.getTime() === today.getTime()) {
                 if (!selectedDate.time) {
@@ -119,7 +119,7 @@ export const Formulario = () => {
                     const selectedHour = parseInt(selectedDate.time, 10);
                     const selectedTime = new Date();
                     selectedTime.setHours(selectedHour, 0, 0, 0); // Establecer la hora seleccionada
-        
+
                     if (selectedTime.getTime() < now.getTime()) {
                         newErrors.time = "La hora seleccionada no puede ser en el pasado.";
                     }
@@ -128,14 +128,14 @@ export const Formulario = () => {
         } else {
             newErrors.date = "Selecciona una fecha.";
         }
-        
+
         if (!selectedDate.time) {
             newErrors.time = "Selecciona una hora.";
         }
-        
-    
+
+
         setErrors(newErrors);
-        return Object.keys(newErrors).length === 0; 
+        return Object.keys(newErrors).length === 0;
     };
 
     const handleSubmit = async (event) => {
@@ -177,7 +177,7 @@ export const Formulario = () => {
 
             {!isLoading && (
                 <div className="container">
-                    <h2 className="text-center">Agenda tu hora aquí</h2>
+                    <h2 className="text-center" style={{ color: "#00BFFF" }}>Agenda tu hora aquí</h2>
                     <div className="form-container">
                         <form className="form" onSubmit={handleSubmit}>
                             <div className="row">
@@ -241,32 +241,38 @@ export const Formulario = () => {
                             <div className="mb-3">
                                 <label htmlFor="fechaHora" className="form-label">Fecha y Hora</label>
                                 <div className="d-flex">
-                                    <input
-                                        type="date"
-                                        id="fecha"
-                                        className="form-control me-2"
-                                        value={selectedDate.date}
-                                        onChange={(e) => setSelectedDate({ ...selectedDate, date: e.target.value })}
-                                        onBlur={() => handleBlur('date')}
-                                        required
-                                    />
-                                    <select
-                                        id="hora"
-                                        className="form-control"
-                                        value={selectedDate.time || ""}
-                                        onChange={(e) => setSelectedDate({ ...selectedDate, time: e.target.value })}
-                                        onBlur={() => handleBlur('time')}
-                                        required
-                                    >
-                                        <option value="" disabled>Selecciona la hora</option>
-                                        {Array.from({ length: 15 }, (_, i) => i + 9).map(hour => (
-                                            <option key={hour} value={hour}>{hour}:00</option>
-                                        ))}
-                                    </select>
+                                    <div className="me-2 w-50">
+                                        <input
+                                            type="date"
+                                            id="fecha"
+                                            className="form-control"
+                                            value={selectedDate.date}
+                                            onChange={(e) => setSelectedDate({ ...selectedDate, date: e.target.value })}
+                                            onBlur={() => handleBlur('date')}
+                                            required
+                                        />
+                                        {errors.date && <p className="text-danger mt-1">{errors.date}</p>}
+                                    </div>
+
+                                    <div className="w-50">
+                                        <select
+                                            id="hora"
+                                            className="form-control"
+                                            value={selectedDate.time || ""}
+                                            onChange={(e) => setSelectedDate({ ...selectedDate, time: e.target.value })}
+                                            onBlur={() => handleBlur('time')}
+                                            required
+                                        >
+                                            <option value="" disabled>Selecciona la hora</option>
+                                            {Array.from({ length: 15 }, (_, i) => i + 9).map(hour => (
+                                                <option key={hour} value={hour}>{hour}:00</option>
+                                            ))}
+                                        </select>
+                                        {errors.time && <p className="text-danger mt-1">{errors.time}</p>}
+                                    </div>
                                 </div>
-                                {errors.date && <p className="text-danger">{errors.date}</p>}
-                                {errors.time && <p className="text-danger">{errors.time}</p>}
                             </div>
+
 
                             <button
                                 type="submit"
